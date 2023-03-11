ec2d0fd3536d21efc87d7e89f4bfc35f1328a124
Basirahamnov63 <113968574+Brzrar@users.noreply.github.com>
2023-03-11 16:34:45 +0600

    hw2
    hw2

---
+++ h2w/app.js
@@ -0,0 +1,27 @@
+let Starbucks = {
+  coffee: {
+    americano: "Одинарный эспрессо с горячей водой",
+    latte: "Двойной эспрессо с молоком",
+    cappuccino: "Тройной эспрессо с равными долями молока и молочной пены",
+    espresso: "Крепкий черный кофе"
+  },
+  tea: {
+    black: "Черный чай",
+    green: "Зеленый чай",
+    herbal: "Травяной чай",
+  }
+}
+
+let deduc = prompt("Что вы хотели заказать?").trim().toLowerCase();
+
+if (Starbucks.coffee[deduc]) {
+  if (Starbucks.coffee[deduc] !== false) {
+    console.log(`Вы заказали ${deduc}. ${Starbucks.coffee[deduc]}`);
+  } else {
+    console.log(`Извините, ${deduc} нет в наличии.`);
+  }
+} else if (Starbucks.tea[deduc]) {
+  console.log(`Вы заказали ${deduc}. ${Starbucks.tea[deduc]}`);
+} else {
+  console.log(`Извините, ${deduc} нет в наличии.`);
+}
